3c31bc04391df3a321e7596f4dd2f6de6575cac3
Ali Raza <aliraza137725@gmail.com>
2023-09-27 11:42:15 +0500

    Now new function Edit is added in project and works on todo

--- src/Pages/Frontend/Hero/Hero.js
+++ src/Pages/Frontend/Hero/Hero.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import { collection, getDocs } from "firebase/firestore";
 import { db } from '../../../config/firebase';
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, serverTimestamp, setDoc } from "firebase/firestore";
 
-
+let initialstate = {
+    title: "",
+    location: "",
+    description: ""
+}
 export default function Todo() {
 
-
+    const [editTodo, setEditTodo] = useState(initialstate);
     const [documents, setDocuments] = useState([]);
     const [isLoading, setIsLoading] = useState(false)
+    const [isProcessingUpdate, setIsProcessingUpdate] = useState(false)
 
 
+    const handlechange = (e) => {
+        setEditTodo(s => ({ ...s, [e.target.name]: e.target.value }))
+    }
 
     const fetchDocuments = async () => {
         setIsLoading(true)
@@ -46,7 +54,36 @@ export default function Todo() {
         }
     }
 
-
+    const handleUpdate = async () => {
+        const todo = { ...editTodo }
+        todo.dateCreated = todo.dateCreated
+        todo.dateModified = serverTimestamp()
+        // todo.modifiedBy = {
+        //     email: user.email,   
+        //     uid: user.uid
+        // }
+        setIsProcessingUpdate(true)
+        try {
+            // 1st args database, 2nd args collection,3rd args data.id ,4th args data
+            await setDoc(doc(db, "todos", todo.id), todo, { merge: true });
+            // window.toastify('Todo is Updated Successfully', 'success')
+            // This is used by replace old todo by updated todo with refreshing the page
+            alert("Todo is updated successfully")
+            //logic start
+            let newDocuments = documents.map((doc) => {
+                if (doc.id === todo.id)
+                    return todo
+                return doc
+            })
+            setDocuments(newDocuments)
+            //logic end
+        }
+        catch (err) {
+            console.log(err)
+            // window.toastify('something gone error', 'error')
+        }
+        setIsProcessingUpdate(false)
+    }
 
     return (
         <>
@@ -60,8 +97,8 @@ export default function Todo() {
                         </div>
                         <div className="row">
                             <div className="col ">
-                                <div class="table-responsive">
-                                    <table class="table">
+                                <div className="table-responsive">
+                                    <table className="table">
                                         <thead>
                                             <tr>
                                                 <th scope="col">Sr. No</th>
@@ -78,15 +115,47 @@ export default function Todo() {
                                                     <td>{todo.title}</td>
                                                     <td>{todo.location}</td>
                                                     <td>{todo.description}</td>
-                                                    <td><button className='btn btn-info btn-sm me-1'>Edit</button><button className='btn btn-danger btn-sm ' onClick={() => { handleTodoDelete(todo.id) }}>Delete</button></td>
+                                                    <td><button className='btn btn-info btn-sm me-1' type='button' onClick={() => { setEditTodo(todo) }} data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button><button className='btn btn-danger btn-sm ' onClick={() => { handleTodoDelete(todo.id) }}>Delete</button></td>
                                                 </tr>
                                             })
-                                            }
-
+                                              }
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* modal */}
+            <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title text-center" id="exampleModalLabel">Update Your Todo </h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <div className="row mb-3">
+                                <div className="col">
+                                    <input type="text" id='title' name='title' className='w-100 py-2  px-3' onChange={handlechange} value={editTodo.title} placeholder='Enter your Title' />
+                                </div>
+                                <div className="col">
+                                    <input type="text" id='location' name='location' className='w-100 py-2 px-3' onChange={handlechange} value={editTodo.location} placeholder='Enter your Location' />
+                                </div>
+                            </div>
+                            <div className="row ">
+                                <div className="col">
+                                    <textarea name="description" className='w-100 px-3 py-3' id="description" cols="30" rows="10" onChange={handlechange} value={editTodo.description} placeholder='Enter your Description '></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={handleUpdate} disabled={isProcessingUpdate}>{
+                                !isProcessingUpdate ? "Update" :
+                                    <div className="spinner-border spinner-border-sm"></div>
+                            }</button>
                         </div>
                     </div>
                 </div>
