f7694b1f03d94f207c7401ea0943cd37f1706735
Ali Raza <aliraza137725@gmail.com>
2023-10-01 22:08:29 +0500

    some changes are done

--- src/Pages/Auth/SignUp/index.js
+++ src/Pages/Auth/SignUp/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import SignUp from './Signup'
+import SignUp from '../SignUp/SignUp'
 
 export default function index() {
   return (
--- src/Pages/Routes.js
+++ src/Pages/Routes.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Frontend from './Frontend';
-// import Auth from './Auth'
+import Auth from './Auth'
 
 export default function Index() {
     return (
         <>
             <Routes>
                 <Route path='/*' element={<Frontend />} />
-                {/* <Route path='/auth/*' element={<Auth/>}/> */}
+                <Route path='/auth/*' element={<Auth/>}/>
             </Routes>
         </>
     )
